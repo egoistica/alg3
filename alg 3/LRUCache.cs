b72bbc86429af4979943f4f6ec078b72cbae1fb9
Artem <bobrovod174@mail.ru>
2025-10-21 17:29:17 +0500

    result + fix

--- alg 3/LRUCache.cs
+++ alg 3/LRUCache.cs
@@ -1,26 +1,24 @@
 // Пример применения Списка: LRU (Least Recently Used) Cache
 // Применение: кэширование в веб-серверах, базах данных, операционных системах
-public class LRUCache<TKey, TValue>
+public class LRUCache<TKey, TValue> where TKey : notnull
 {
     private readonly int _capacity;
-    private readonly Dictionary<TKey, LinkedListNode<CacheItem<TKey, TValue>>> _cache;
-    private readonly LinkedList<CacheItem<TKey, TValue>> _accessOrder;
+    private readonly Dictionary<TKey, CacheNode<TKey, TValue>> _cache;
+    private readonly DoublyLinkedList<TKey, TValue> _list;
 
     public LRUCache(int capacity)
     {
         _capacity = capacity;
-        _cache = new Dictionary<TKey, LinkedListNode<CacheItem<TKey, TValue>>>();
-        _accessOrder = new LinkedList<CacheItem<TKey, TValue>>();
+        _cache = new Dictionary<TKey, CacheNode<TKey, TValue>>();
+        _list = new DoublyLinkedList<TKey, TValue>();
     }
 
-    public TValue Get(TKey key)
+    public TValue? Get(TKey key)
     {
         if (_cache.TryGetValue(key, out var node))
         {
-            // Перемещаем узел в начало (самый недавно использованный)
-            _accessOrder.Remove(node);
-            _accessOrder.AddFirst(node);
-            return node.Value.Value;
+            _list.MoveToFront(node);
+            return node.Value;
         }
         return default(TValue);
     }
@@ -29,26 +27,23 @@ public class LRUCache<TKey, TValue>
     {
         if (_cache.TryGetValue(key, out var existingNode))
         {
-            // Обновляем существующий элемент
-            existingNode.Value.Value = value;
-            _accessOrder.Remove(existingNode);
-            _accessOrder.AddFirst(existingNode);
+            existingNode.Value = value;
+            _list.MoveToFront(existingNode);
         }
         else
         {
-            // Добавляем новый элемент
             if (_cache.Count >= _capacity)
             {
-                // Удаляем самый старый элемент (последний в списке)
-                var lastNode = _accessOrder.Last;
-                _cache.Remove(lastNode.Value.Key);
-                _accessOrder.Remove(lastNode);
+                var lastNode = _list.RemoveLast();
+                if (lastNode != null)
+                {
+                    _cache.Remove(lastNode.Key);
+                }
             }
 
-            var newNode = new LinkedListNode<CacheItem<TKey, TValue>>(
-                new CacheItem<TKey, TValue> { Key = key, Value = value });
+            var newNode = new CacheNode<TKey, TValue>(key, value);
+            _list.AddToFront(newNode);
             _cache[key] = newNode;
-            _accessOrder.AddFirst(newNode);
         }
     }
 
@@ -62,19 +57,98 @@ public class LRUCache<TKey, TValue>
     public void PrintCache()
     {
         Console.WriteLine("LRU Cache содержимое (от недавнего к старому):");
-        var current = _accessOrder.First;
+        _list.Print();
+    }
+}
+
+public class CacheNode<TKey, TValue>
+{
+    public TKey Key { get; set; }
+    public TValue Value { get; set; }
+    public CacheNode<TKey, TValue>? Next { get; set; }
+    public CacheNode<TKey, TValue>? Prev { get; set; }
+
+    public CacheNode(TKey key, TValue value)
+    {
+        Key = key;
+        Value = value;
+    }
+}
+
+public class DoublyLinkedList<TKey, TValue>
+{
+    private CacheNode<TKey, TValue>? _head;
+    private CacheNode<TKey, TValue>? _tail;
+
+    public void AddToFront(CacheNode<TKey, TValue> node)
+    {
+        node.Next = _head;
+        node.Prev = null;
+
+        if (_head != null)
+        {
+            _head.Prev = node;
+        }
+
+        _head = node;
+
+        if (_tail == null)
+        {
+            _tail = node;
+        }
+    }
+
+    public void MoveToFront(CacheNode<TKey, TValue> node)
+    {
+        if (_head == node) return;
+
+        if (node.Prev != null)
+        {
+            node.Prev.Next = node.Next;
+        }
+
+        if (node.Next != null)
+        {
+            node.Next.Prev = node.Prev;
+        }
+
+        if (_tail == node)
+        {
+            _tail = node.Prev;
+        }
+
+        AddToFront(node);
+    }
+
+    public CacheNode<TKey, TValue>? RemoveLast()
+    {
+        if (_tail == null) return null;
+
+        var lastNode = _tail;
+
+        if (_tail.Prev != null)
+        {
+            _tail.Prev.Next = null;
+            _tail = _tail.Prev;
+        }
+        else
+        {
+            _head = null;
+            _tail = null;
+        }
+
+        return lastNode;
+    }
+
+    public void Print()
+    {
+        var current = _head;
         int index = 1;
         while (current != null)
         {
-            Console.WriteLine($"{index}. {current.Value.Key} = {current.Value.Value}");
+            Console.WriteLine($"{index}. {current.Key} = {current.Value}");
             current = current.Next;
             index++;
         }
     }
-}
-
-public class CacheItem<TKey, TValue>
-{
-    public TKey Key { get; set; }
-    public TValue Value { get; set; }
 }
--- alg 3/obj/Debug/net9.0/alg 3.AssemblyInfo.cs
+++ alg 3/obj/Debug/net9.0/alg 3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("alg 3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44e9cd5af7dd536f72ccc98069dd5087b97944d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c82f82149c264fa4c210605833c790f935d53919")]
 [assembly: System.Reflection.AssemblyProductAttribute("alg 3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("alg 3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
